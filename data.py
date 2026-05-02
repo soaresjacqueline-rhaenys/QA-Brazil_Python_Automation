62762c42a663fae69a8f46bff15c5f96075df40b
Jacqueline Soares <soares.jacqueline@outlook.com>
2026-05-02 15:14:17 -0300

    Projeto do Sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-cd555d71-9669-4ef0-8580-bad5943129d6.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,52 @@
+import data
+import helpers
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+    def test_urban_routes(self):
+        # Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("função criada para definir o modo de corrida")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("função criada para preencher o número de telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("função criada para preencher o cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("função criada para deixar mensagem ao motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("função criada para reservar cobertores ou lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            # Adicionar em S8
+            print("função criada para adicionar a quantidade de sorvete")
+            pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("função criada para pesquisar modelo do carro")
+        pass
